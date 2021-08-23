b169229320d1eaf0328da16346d5430d7f0ff75a
Your Name <30753067+Suraj-Ram@users.noreply.github.com>
2021-08-23 17:05:59 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import Header from "./components/Header";
-import Bio from "./components/Bio";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Header from "./components/Header"
+import Bio from "./components/Bio"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
 
 function App() {
   return (
     <>
       <Header />
       <div className="container mx-auto px-4 py-2">
+        <Hero />
         <Bio />
         <Projects />
         <Contact />
       </div>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import Bio from "./Bio"
+const Hero = () => {
+  return (
+    <div className=" bg-gradient-to-b from-gray-200 to-white border border-red-400">
+      <Bio />
+    </div>
+  )
+}
+
+export default Hero
