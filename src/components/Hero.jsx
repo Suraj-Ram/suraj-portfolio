32eaf47d52b03d7020a9fac54d2a3c2c60107c31
Your Name <30753067+Suraj-Ram@users.noreply.github.com>
2021-08-25 20:13:57 +0400

    Added responsive bottom margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
 //import Bio from "./Bio";
-import { useState } from "react";
-import Blob from "./Blob";
+import { useState } from "react"
+import Blob from "./Blob"
 
 const Hero = () => {
-  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
 
-  window.addEventListener("resize", () => setwindowWidth(window.innerWidth));
+  window.addEventListener("resize", () => setwindowWidth(window.innerWidth))
 
   return (
-    <div className=" bg-gradient-to-b from-gray-300 to-white">
+    <div className=" md:bg-gradient-to-b from-gray-300 to-white">
       <div className="container mx-auto px-4 py-2">
-        <div className="mb-16">
+        <div className="mb-10 md:mb-14 lg:mb-28 xl:mb-32">
           <div className="flex justify-between items-center lg:justify-start lg:gap-4">
             <section className="">
               <h1
-                className="font-bold text-3xl md:text-5xl xl:text-6xl md:pt-4 xl:pt-8 tracking-tight mb-4 lg:mb-8 text-black dark:text-white"
+                className="font-bold text-4xl md:text-5xl xl:text-6xl md:pt-4 xl:pt-8 tracking-tight mb-4 lg:mb-8 text-black dark:text-white"
                 id="title"
               >
                 Hey, I'm Suraj
               </h1>
-              <p className="prose text-gray-600 max-w-xl dark:text-gray-400 mb-14 md:text-lg">
+              <p className="prose text-gray-600 max-w-xl dark:text-gray-400 md:text-lg">
                 My interest area is web development, having developed web apps
                 and websites using HTML, CSS, Vanilla JS, React, Bootstrap and
                 Tailwind. I am fascinated particularly fascinated by AI and
@@ -34,7 +34,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
