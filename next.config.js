5c913af2764d4742d27eac97b31b97a4e44a421c
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2026-01-12 13:44:03 +0400

    add nextjs rewrite, update posthot init to proxy endpoint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/hog_ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/hog_ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
+  skipTrailingSlashRedirect: true,
+}
+
 
 module.exports = nextConfig
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,6 +4,7 @@ console.log("env label")
 console.log(process.env.NEXT_PUBLIC_VERCEL_ENV)
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: "/hog_ingest",
+  ui_host: "https://us.posthog.com",
   defaults: '2025-11-30'
 });
